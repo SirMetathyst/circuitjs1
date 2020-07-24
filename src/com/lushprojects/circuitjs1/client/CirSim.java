d9e3368fedc31aaeb67ea4e0ccc5f2aeacca5f3c
Alexander Hunt <sirmetathyst@outlook.com>
2020-07-24 23:13:13 +0100

    Remove VCVSElm

--- src/com/lushprojects/circuitjs1/client/CirSim.java
+++ src/com/lushprojects/circuitjs1/client/CirSim.java
@@ -839,7 +839,6 @@ MouseOutHandler, MouseWheelHandler {
     	MenuBar activeBlocMenuBar = new MenuBar(true);
     	activeBlocMenuBar.addItem(getClassCheckItem(LS("Add Op Amp (ideal, - on top)"), "OpAmpElm"));
     	activeBlocMenuBar.addItem(getClassCheckItem(LS("Add Analog Switch (SPST)"), "AnalogSwitchElm"));
-    	activeBlocMenuBar.addItem(getClassCheckItem(LS("Add Voltage-Controlled Voltage Source"), "VCVSElm"));
     	activeBlocMenuBar.addItem(getClassCheckItem(LS("Add Voltage-Controlled Current Source"), "VCCSElm"));
     	activeBlocMenuBar.addItem(getClassCheckItem(LS("Add Subcircuit Instance"), "CustomCompositeElm"));
     	mainMenuBar.addItem(SafeHtmlUtils.fromTrustedString(CheckboxMenuItem.checkBoxHtml+LS("&nbsp;</div>Active Building Blocks")), activeBlocMenuBar);
@@ -4352,7 +4351,6 @@ MouseOutHandler, MouseWheelHandler {
     	case 'w': return new WireElm(x1, y1, x2, y2, f, st);
     	case 159: return new AnalogSwitchElm(x1, y1, x2, y2, f, st);
     	case 172: return new VarRailElm(x1, y1, x2, y2, f, st);
-    	case 212: return new VCVSElm(x1, y1, x2, y2, f, st);
     	case 213: return new VCCSElm(x1, y1, x2, y2, f, st);
         }
     	return null;
@@ -4387,8 +4385,6 @@ MouseOutHandler, MouseWheelHandler {
     		return (CircuitElm) new OpAmpElm(x1, y1);
     	if (n=="AnalogSwitchElm")
     		return (CircuitElm) new AnalogSwitchElm(x1, y1);
-    	if (n=="VCVSElm")
-		return (CircuitElm) new VCVSElm(x1, y1);
     	if (n=="VCCSElm")
 		return (CircuitElm) new VCCSElm(x1, y1);
     	return null;
